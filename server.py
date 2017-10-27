a2d921a9c276ca17fed63bf1b8f2022dbf730f89
colleen <elena.colleen@gmail.com>
2017-10-26 21:40:58 -0700

    added more homepage, working on routes

--- server.py
+++ server.py
@@ -7,7 +7,6 @@ import os
 account_sid = os.environ.get("account_sid")
 # Your Auth Token from twilio.com/console
 auth_token = os.environ.get("auth_token")
-
 client = Client(account_sid, auth_token)
 phone_number = os.environ.get("phone_number")
 
@@ -20,25 +19,27 @@ def main():
 
     return render_template("homepage.html")
 
-@app.route("/sms", methods=['GET', 'POST'])
+
+@app.route("/sms", methods=['POST'])
 def incoming_sms():
     """Send a dynamic reply to an incoming text message"""
     # Get the message the user sent our Twilio number
+
     body = request.values.get('Body', None)
 
-    cat_facts = ["Cats use their tails for balance and have nearly 30 individual bones in them! \
-                 <To cancel Daily Cat Facts, reply 'oh god'>",
-                 "In ancient Egypt killing a cat was a crime punishable by death. Thanks for choosing Cat Facts!",
-                 "Did you know there are about 100 distinct breeds of domestic cat? Plenty of furry love!",
-                 "Welcome to Cat Facts! Did you know that the first cat show was held in 1871 at the Crystal Palace in London? Mee-wow!",
-                 "Cats bury their feces to cover their trails from predators."]
-
+    # cat_facts = ["Cats use their tails for balance and have nearly 30 individual bones in them! \
+    #              <To cancel Daily Cat Facts, reply 'oh god'>",
+    #              "In ancient Egypt killing a cat was a crime punishable by death. Thanks for choosing Cat Facts!",
+    #              "Did you know there are about 100 distinct breeds of domestic cat? Plenty of furry love!",
+    #              "Welcome to Cat Facts! Did you know that the first cat show was held in 1871 at the Crystal Palace in London? Mee-wow!",
+    #              "Cats bury their feces to cover their trails from predators."]
+    cat_facts = ['a', 'b', 'c']
     # Start our TwiML response
     resp = MessagingResponse()
 
     # Determine the right reply for this message
     if body == 'hello':
-        resp.message("Meeeow! Welcome to Cat Facts!")
+        resp.message("Meeeow! Welcome to Cat Texts!")
     elif body == 'bye':
         resp.message("Goodnight meow!")
     else:
@@ -47,20 +48,33 @@ def incoming_sms():
 
     return str(resp)
 
+@app.route("/welcome", methods=['GET'])
+def get_info():
+
+    cat_name = request.args.get("cat-name")
+
+    return cat_name
+
+@app.route("/welcome2", methods=['POST'])
 def welcome():
-    """Welcome to the user to Cat Facts"""
+    """Welcome to the user to Cat Texts"""
+
+    # cat_name = request.args.get("cat-name")
+    # print type(cat_name)
+    # print cat_name, "stuff"
+    cat_name = get_info()
 
     message = client.messages.create(
     to=phone_number, 
     from_="+14138486585",
     media_url="https://static.pexels.com/photos/62321/kitten-cat-fluffy-cat-cute-62321.jpeg",
-    body="Thanks for signing up for Cat Facts! You will now receive daily fun facts about CATS! >o< \
-          Reply 'hello' to say hi, 'bye' to say goodnight, and anything else to get a CAT FACT!")
+    # body="Thanks for signing up for Cat Texts! Reply 'hello' to say hi, 'bye' to say goodnight, and anything else to get a CAT TEXT!")
+    body=cat_name)
 
     print(message.sid)
 
 if __name__ == "__main__":
-    welcome()
+    # welcome()
     app.run(port=5000, host='0.0.0.0')
 
 
--- static/facts.js
+++ static/facts.js
@@ -3,6 +3,14 @@
 function getTexts(evt){
     evt.preventDefault()
     console.log("hi");
-    $.post('/sms');
+    $.post('/welcome');
 }
 $('#cat-btn').on('click', getTexts);
+
+
+function getName(evt){
+    evt.preventDefault()
+    console.log("hi2");
+    $.post('/welcome');
+}
+$('#cat-btn').on('click', getName);
