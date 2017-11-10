01098b9d2364de5cb2d0ce5b39df3f101257d848
vagrant <vagrant@vagrant.vm>
2017-11-10 00:27:43 +0000

    added registration and login

--- model.py
+++ model.py
@@ -6,12 +6,32 @@ db = SQLAlchemy()
 
 #*****************************************************************************#
 
+class User(db.Model):
+    """User model"""
+
+    __tablename__ = "users"
+
+    user_id = db.Column(db.Integer, autoincrement=True, primary_key=True, nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(20), nullable=False)
+
+    def __repr__(self):
+        """Provide useful info when printed to console"""
+
+        s = "<User user_id=%s username=%s password=%s>"
+
+        # TODO probably remove password when not in development
+        return s % (self.user_id, self.username, self.password)
+
 class Cat(db.Model):
     """Cat model"""
 
     __tablename__ = "cats"
 
     cat_id = db.Column(db.Integer, autoincrement=True, primary_key=True, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"), nullable=False) # foreign key to users
+    username = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), nullable=False)
     name = db.Column(db.String(20), nullable=False)
     dinner_time = db.Column(db.String(5), nullable=False)
     snack = db.Column(db.String(20), nullable=False)
@@ -24,6 +44,8 @@ class Cat(db.Model):
     # but I guess I could store my own number instead of sourcing from environ
     # to get it started
 
+    user = db.relationship("User", backref=db.backref("cats"))
+
     def __repr__(self):
         """Provide useful info when printed to console"""
 
@@ -32,6 +54,28 @@ class Cat(db.Model):
         return s % (self.cat_id, self.name)
 
 #*****************************************************************************#
+
+def connect_to_db(app):
+    """Connect the database to our Flask app."""
+
+    # Configure to use our Postgres database
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cattexts'
+    app.config['SQLALCHEMY_ECHO'] = True
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.app = app
+    db.init_app(app)
+
+
+if __name__ == "__main__":
+
+    from flask import Flask
+
+    app = Flask(__name__)
+
+    connect_to_db(app)
+    print "Connected to DB."
+
+    db.create_all()
 
 # def init_app():
 
@@ -49,29 +93,3 @@ class Cat(db.Model):
 #     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 #     db.app = app
 #     db.init_app(app)
-
-
-def connect_to_db(app):
-    """Connect the database to our Flask app."""
-
-    # Configure to use our Postgres database
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cattexts'
-    app.config['SQLALCHEMY_ECHO'] = True
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    db.app = app
-    db.init_app(app)
-
-
-if __name__ == "__main__":
-    # As a convenience, if we run this module interactively, it will leave
-    # you in a state of being able to work with the database directly.
-
-    # So that we can use Flask-SQLAlchemy, we'll make a Flask app
-    from flask import Flask
-
-    app = Flask(__name__)
-
-    connect_to_db(app)
-    print "Connected to DB."
-
-    db.create_all()
