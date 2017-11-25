ff0c082a3cb35b5ae41b8995ca8b478922831635
vagrant <elena.colleen@gmail.com>
2017-11-25 00:32:55 +0000

    Working on daily texts

--- daily.py
+++ daily.py
@@ -4,7 +4,11 @@ from twilio.rest import Client
 import os
 import schedule
 import time
-from server import CAT_INFO
+from model import connect_to_db, db, User, Cat
+
+from datetime import datetime
+from pytz import timezone
+
 # Your Account SID from twilio.com/console
 account_sid = os.environ.get("account_sid")
 # Your Auth Token from twilio.com/console
@@ -14,24 +18,40 @@ phone_number = os.environ.get("phone_number")
 
 app = Flask(__name__)
 
-print CAT_INFO
-
-# @app.route("/sms", methods=['POST'])
 def daily_text():
 
     message = client.messages.create(
     to=phone_number, 
     from_="+14138486585",
     # media_url="https://static.pexels.com/photos/62321/kitten-cat-fluffy-cat-cute-62321.jpeg",
-    body="Hi, " + CAT_INFO['cat_name'] + "here. I'm pretty sure it's dinner time!")
-    # body = "hi I'm working")
+    body="Hi, " + name + " here. I'm pretty sure it's time to feed me!!")
 
     print(message.sid)
 
-# schedule.every().day.at(CAT_INFO['dinner_time']).do(daily_text)
-schedule.every(5).seconds.do(daily_text)
 
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+if __name__ == "__main__":
+    connect_to_db(app)
+
+    user_id = User.query.filter_by(phone_number=phone_number).first()
+    user_id = user_id.user_id
+    dinner_time = db.session.query(Cat.dinner_time).filter(User.user_id==user_id).first()
+    dinner_time = str(dinner_time[0])
+    # TODO need to convert dinner_time to UTC
+    # dinner_time = "00:17"
+    print dinner_time
+    name = db.session.query(Cat.name).filter(User.user_id==user_id).first()
+    name = str(name[0])
+    print name
+    schedule.every().day.at(dinner_time).do(daily_text)
+    # schedule.every(5).seconds.do(daily_text)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+
+    # Current time in UTC
+    now_utc = datetime.now(timezone('UTC'))
+    # Convert to US/Pacific time zone
+    now_pacific = now_utc.astimezone(timezone('US/Pacific'))
 
