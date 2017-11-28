d3e1f9be7702055e13e56131e706aa320144e481
vagrant <vagrant@vagrant.vm>
2017-11-28 03:29:09 +0000

    Working on getting texts running asynchronously for multiple users

--- daily.py
+++ daily.py
@@ -2,22 +2,19 @@ from flask import Flask, request, redirect, render_template
 # from twilio.twiml.messaging_response import MessagingResponse
 from twilio.rest import Client
 import os
+import threading
 import schedule
 import time
 from model import connect_to_db, db, User, Cat
-
-from datetime import datetime
-from pytz import timezone
-import pytz
 
 # Your Account SID from twilio.com/console
 account_sid = os.environ.get("account_sid")
 # Your Auth Token from twilio.com/console
 auth_token = os.environ.get("auth_token")
 client = Client(account_sid, auth_token)
-phone_number = os.environ.get("phone_number")
-
+# phone_number = os.environ.get("phone_number") # my phone number
 app = Flask(__name__)
+################################################################################
 
 def daily_text():
 
@@ -33,45 +30,49 @@ def daily_text():
 if __name__ == "__main__":
     connect_to_db(app)
 
-    # TODO currently this is pulling my phone number from secrets.sh
-    # and getting the first entry, so it's always user id 1
-    # should change to query db for all phone numbers and do this for everyone
-    # ...even though technically everything in the db is my phone number
-    # since I have the trial Twilio account. could shoot off texts at different
-    # times for testing purposes so I'm not waiting a full 24 hours 
+    for user in User.query.all():
+        user_id = user.user_id
+        for cat in Cat.query.filter_by(user_id=user_id).all():
+            dinner_time = db.session.query(Cat.dinner_time).filter(User.user_id==user_id).first()
+            # if multiple cats (not currently supported) i think .first
+            # may not be the way to go, but .one() is throwing an error which is weird
+            minutes = dinner_time[0].minute
+            hour = dinner_time[0].hour
 
-    user_id = User.query.filter_by(phone_number=phone_number).first()
-    user_id = user_id.user_id
-    dinner_time = db.session.query(Cat.dinner_time).filter(User.user_id==user_id).first()
+            if len(str(hour)) < 2:
+                new_hour = "0"
+                new_hour += str(hour)
+            else:
+                new_hour = str(hour)
 
-    minutes = dinner_time[0].minute
-    hour = dinner_time[0].hour
+            if len(str(minutes)) < 2:
+                new_minutes = "0"
+                new_minutes += str(minutes) 
+            else:
+                new_minutes = str(minutes)
 
-    if len(str(hour)) < 2:
-        new_hour = "0"
-        new_hour += str(hour)
-    else:
-        new_hour = str(hour)
+            this_time = new_hour + ":" + new_minutes
+            name = db.session.query(Cat.name).filter(User.user_id==user_id).first()
+            name = str(name[0])
+            phone_number = user.phone_number
 
-    if len(str(minutes)) < 2:
-        new_minutes = "0"
-        new_minutes += str(minutes)
-    else:
-        new_minutes = str(minutes)
+            print cat.name
+            print cat.dinner_time
+            print user.phone_number
 
-    this_time = new_hour + ":" + new_minutes
-    print this_time
-    print type(this_time)
-    name = db.session.query(Cat.name).filter(User.user_id==user_id).first()
-    name = str(name[0])
-    print name
-    schedule.every().day.at(this_time).do(daily_text)
-    # schedule.every().day.at("01:24").do(daily_text)
-    # schedule.every(5).seconds.do(daily_text)
+            # need to find way to run these asynchronously, currently it does the 
+            # first one and stays there forever. how can I run the others?
+            # use threading? need to read more about this
 
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
+            schedule.every().day.at(this_time).do(daily_text)
+            # can't pass variables to daily_text it seems
+            # this doesn't work if i pass phone_number, for example
+
+            while True:
+                schedule.run_pending()
+                time.sleep(1)
+
+
 
 
 
