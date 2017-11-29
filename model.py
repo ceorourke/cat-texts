4e1faa891edbdb13506908644266ad13802de38c
vagrant <vagrant@vagrant.vm>
2017-11-29 08:39:07 +0000

    Working on updating timezone in update route

--- model.py
+++ model.py
@@ -13,16 +13,16 @@ class User(db.Model):
 
     user_id = db.Column(db.Integer, autoincrement=True, primary_key=True, nullable=False)
     email = db.Column(db.String(50), nullable=False)
+    timezone = db.Column(db.String(100), nullable=False)    
     phone_number = db.Column(db.String(20), nullable=False)
     password = db.Column(db.String(200), nullable=False)
 
     def __repr__(self):
         """Provide useful info when printed to console"""
 
-        s = "<User user_id=%s username=%s password=%s>"
+        s = "<User user_id=%s email=%s timezone=%s>"
 
-        # TODO probably remove password when not in development
-        return s % (self.user_id, self.email, self.password)
+        return s % (self.user_id, self.email, self.timezone)
 
 class Cat(db.Model):
     """Cat model"""
@@ -32,7 +32,6 @@ class Cat(db.Model):
     cat_id = db.Column(db.Integer, autoincrement=True, primary_key=True, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"), nullable=False) # foreign key to users
     name = db.Column(db.String(20), nullable=False)
-    # dinner_time = db.Column(db.String(5), nullable=False)
     dinner_time = db.Column(db.DateTime, nullable=False)
     snack = db.Column(db.String(20), nullable=False)
     activity1 = db.Column(db.String(20), nullable=False)
