12fad27719b7b77b4c8a045848c5d7db5dd63f29
vagrant <elena.colleen@gmail.com>
2018-01-12 03:28:39 +0000

    turned debug mode back off

--- server.py
+++ server.py
@@ -447,7 +447,7 @@ def incoming_sms():
 
 if __name__ == "__main__":
 
-    app.debug = True
+    # app.debug = True
     app.jinja_env.auto_reload = app.debug  # make sure templates, etc. are not cached in debug mode
 
     connect_to_db(app, location="postgres:///cattexts")
